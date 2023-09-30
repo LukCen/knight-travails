53495f38f4d41fafcd24d2490cf1b627578d193e
Lukasz Cena <lbjcena@gmail.com>
2023-09-30 21:18:43 +0200

    first iteration of the loop
    old comments removed, unnecessary Knight class file removed, first
    iteration of the loop introduced

--- knightTravails.js
+++ knightTravails.js
@@ -1,5 +1,4 @@
 'use strict'
-import { Knight } from './Knight.js'
 class Traversal {
   /**
      * @param {Number} x - x coordinate of the knight
@@ -11,57 +10,52 @@ class Traversal {
     this.xDir = [-1, 1, -1, 1, -2, -2, 2, 2] // LEFT-RIGHT
     this.yDir = [2, 2, -2, -2, 1, -1, 1, -1] // UP-DOWN
   }
-
   /**
-     *
-     * @param {Number} x - coords of the knight
-     * @param {Number} y - coords of the knight
-      *@returns {Array<Number>}values of all neighbouring cells
-     */
-  showAllPossible (x, y) { // testing method, for brainstorming
-    const queue = []
-    while (queue.length === 0) {
-      for (let i = 0; i < this.xDir.length && i < this.yDir.length; i++) { // this condition assumes the board is infinite in size, additional checks would be needed if said size was strictly limited
-        queue.push([x + this.xDir[i], y + this.yDir[i]])
-      }
-    }
-    return queue
-  }
+  * @param {Number} targetX - searched coordinate on the X axis
+  * @param {Number} targetY - searched coordinate on the Y axis
+  * @returns {Number} number of moves it takes to reach the target
+  */
 
   calculateMoves (targetX = 0, targetY = 0) {
     const viableMoves = []
-    const queue = []
+    let queue = [[0, 0]]
+    const visited = new Set()
+    let steps = 0
     for (let i = 0; i < this.xDir.length && i < this.yDir.length; i++) {
       viableMoves.push([(this.x + this.xDir[i]), (this.y + this.yDir[i])]) // this will correctly calculate viable moves I think? sure seems to work in the console
     }
+    queue = [...viableMoves] // populate the queue with moves the knight can make at this moment
+    while (queue.length) {
+      const next = []
+      while (queue.length) {
+        const current = queue.shift()
+        const currentX = current[0] // extract current position from the queue
+        const currentY = current[1] // extract current position from the queue
+        console.log(`X: ${currentX} | Y: ${currentY}`)
 
-    const visited = new Set()
-    const steps = 0
-    queue.push(...viableMoves)
-    this.x = queue.shift()[0]
-    this.y = queue.shift()[1]
-    // while (queue.length === 0) {
-    //   if (this.x === targetX && this.y === targetY) { // means knight has reached his destination
-    //     return steps
-    //   } else {
-    //   }
-    // }
-    // console.log(queue)
-    // console.log(viableMoves)
+        if (currentX === targetX && currentY === targetY) { // we've found our goal if this is truthy
+          return steps
+        }
+        for (const d of viableMoves) { // is this even needed in this loop? will investigate later
+          const nextX = currentX + d[0]
+          const nextY = currentY + d[0]
+
+          if (!visited.has(`${nextX},${nextY}`)) { // this entire part might have to be rewritten, I barely understand wtf this is
+            visited.add(`${nextX},${nextY}`)
+            next.push([nextX, nextY])
+          }
+        }
+      }
+      steps++
+      queue = next
+    }
   }
 }
 
-/**
- * 1) create an array composed of arrays of valid X and Y coordinates, using xDir and yDir (maybe unnecessary? will poke at different ideas later)
- * 2) create a set that collects already visited coordinates (prevents duplicates)
- * 3) create a variable to count the number of moves made
- * 4) run a loop of some kind to check if knightX is equal to targetX and knightY is equal to targetY
- * 4a) if it is, return the number of moves, as loop has reached its goal
- * 4b) if not, ...?
- */
+const test = new Traversal(0, 0)
 
-const test = new Traversal(1, 1)
-// console.log(test.xDir[0])
-// console.log(test.showAllPossible(4, 4))
-console.log(test.calculateMoves())
-console.log([test.x, test.y])
+console.log(test.calculateMoves(8, 8)) // these values cause an infinite loop. I think I need to visit all the neighbouring nodes to the one I'm currently at?
+
+/**
+ * I think I'll experiment with keeping an eye of the current position, and if it's values are higher than the target... end the loop i guess? Re-run it with different parameters?
+*/
